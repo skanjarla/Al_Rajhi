08ca269400d9a6d021ba378c2a5dcb60abbcadd8
skanjarla <skanjarla@zyclyx.com>
2021-05-04 14:51:24 +0530

    Add port

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const PizZip = require("pizzip");
 const Docxtemplater = require("docxtemplater"); 
 const cors = require("cors"); 
 
-const port = 8080; 
+const port = process.env.PORT || 8080; 
 app.use(express.static("App"));
 app.use(express.json());
 app.use(cors());
